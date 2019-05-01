2cb9c9aba31d3757c7dcc7c0d36583d446b4902b
tomsure <491583156@qq.com>
2019-05-02 00:00:43 +0800

    路由

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Abs from './Abs'
+
 import ReactRouter from './ReactRouter/index'
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Abs />
+         
          <ReactRouter />
       </div>
     );
--- new-app/src/index.js
+++ new-app/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Abs from './Abs'
+import ReactRouter from './ReactRouter/index'
+
 
 
 ReactDOM.render(
-  <App />,
+  // <App />,
+  // <Abs />,
+   <ReactRouter />,
   document.getElementById('root')
 );
